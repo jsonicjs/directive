fec2a639dfc38ac2db7877dfa763ad7cc3812e80
Richard Rodger <richard@ricebridge.com>
2022-12-16 18:19:50 +0000

    annotate

--- test/quick.js
+++ test/quick.js
@@ -101,62 +101,104 @@ const { Directive } = require('..')
 // console.log(j('add<1,2>'))
 
 
-const SRC = {
-  a: 'A',
-  b: {b:1},
-  c: [2,3],
-}
+// const SRC = {
+//   a: 'A',
+//   b: {b:1},
+//   c: [2,3],
+// }
+
+// const j = Jsonic.make().use(Debug,{trace:true}).use(Directive, {
+//   name: 'constant',
+//   open: '@',
+//   rules: {
+//     open: 'val,pair'
+//   },
+//   action: (rule) => {
+//     // console.log('DA', rule.d, rule.name, rule.child.node, rule.parent.name)
+//     let srcname = ''+rule.child.node
+//     let src = SRC[srcname]
+//     let from = rule.parent.name
+    
+//     if('pair' === from) {
+//       Object.assign(rule.parent.node, src)
+//     }
+//     else {
+//       rule.node = src
+//     }
+//   },
+//   custom: (jsonic, {OPEN, name}) => {
+
+//     // Handle special case of @foo first token - assume a map
+//     jsonic
+//       .rule('val', (rs) => {
+// 	rs.open({
+// 	  s: [OPEN],
+// 	  c: (r)=>0===r.d,
+// 	  p: 'map',
+// 	  b: 1,
+// 	  n: { [name+'_top']:1 }
+// 	})
+//       })
+//       .rule('map', (rs) => {
+// 	rs.open({
+// 	  s: [OPEN],
+// 	  c: (r)=> (1===r.d && 1===r.n[name+'_top']),
+// 	  p: 'pair',
+// 	  b: 1,
+// 	})
+//       })
+//   }
+// })
+
+
+// console.log(j('a:@a'))
+// console.log(j('c:@c'))
+// console.log(j('a:@a c:@c'))
+// console.log(j('@b'))
+// console.log(j('a:1 @b'))
+// console.log(j('a:1 @b c:2'))
+// console.log(j('b:@b'))
+
+// console.log(j('a:@[1]'))
+
 
 const j = Jsonic.make().use(Debug,{trace:true}).use(Directive, {
-  name: 'constant',
+  name: 'annotate',
   open: '@',
   rules: {
-    open: 'val,pair'
+    open: 'val'
   },
   action: (rule) => {
     // console.log('DA', rule.d, rule.name, rule.child.node, rule.parent.name)
-    let srcname = ''+rule.child.node
-    let src = SRC[srcname]
-    let from = rule.parent.name
-    
-    if('pair' === from) {
-      Object.assign(rule.parent.node, src)
-    }
-    else {
-      rule.node = src
-    }
+    rule.parent.use.note = '<'+rule.child.node+'>'
   },
   custom: (jsonic, {OPEN, name}) => {
 
     // Handle special case of @foo first token - assume a map
     jsonic
-      .rule('val', (rs) => {
-	rs.open({
-	  s: [OPEN],
-	  c: (r)=>0===r.d,
-	  p: 'map',
-	  b: 1,
-	  n: { [name+'_top']:1 }
-	})
+      .rule('annotate', (rs) => {
+	rs
+	  .close([
+	    {
+	      r: 'val',
+	      g: 'replace',
+	    }
+	  ])
+	  .ac((r,c,next)=>{
+	    console.log('AC PARENT', r.parent.i, r.parent.name)
+	    console.log('AC NEXT', next.i, next.name)
+	    r.parent.child = next
+	  })
+	  
       })
-      .rule('map', (rs) => {
-	rs.open({
-	  s: [OPEN],
-	  c: (r)=> (1===r.d && 1===r.n[name+'_top']),
-	  p: 'pair',
-	  b: 1,
+      .rule('val', (rs) => {
+	rs.bc((r)=>{
+	  if(r.use.note) {
+	    r.node['@']=r.use.note 
+	  }
 	})
       })
   }
 })
 
-
-console.log(j('a:@a'))
-console.log(j('c:@c'))
-console.log(j('a:@a c:@c'))
-console.log(j('@b'))
-console.log(j('a:1 @b'))
-console.log(j('a:1 @b c:2'))
-console.log(j('b:@b'))
-
-console.log(j('a:@[1]'))
+console.log(j('[@a {x:1}]'))
